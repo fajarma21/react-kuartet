12061c270db5217f80744ff851e7dbb9661594ca
Fajar Maulana <fajarma21@gmail.com>
2025-06-19 15:26:45 +0700

    feat(config): change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-quartet/',
+  base: '/react-kuartet/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
